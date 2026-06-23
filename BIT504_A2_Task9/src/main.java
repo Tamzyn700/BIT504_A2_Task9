7f1175ffef3b208d3b7df82b31954aadfc7a50d1
BlueSkye-20 <tamzyn.koen@gmail.com>
2026-06-23 17:41:29 +1200

    Added update1 message

---
+++ BIT504_A2_Task9/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Task 9 - Version Control");
+        System.out.println("First version of the project");
+        System.out.println("Added feature update #1");
+	}
+
+}
